9f7e061204fd29ce422a4a3430ed031578dc70de
Simon Tudge <sjtudge@gmail.com>
2015-12-14 17:39:37 +0000

    First commit, setup successful

---
+++ tests/test_dummy.py
@@ -0,0 +1,16 @@
+##A dummy test to serve as a template for future testing, and to test that the integration works correctly.
+
+import unittest
+
+class test_dummy(unittest.TestCase):
+
+	def setUp(self):
+		"""Called on setup"""
+		pass
+
+	def tearDown(self):
+		"""Called after tests have run"""
+		pass
+
+	def test_dummy_test(self):
+		self.assertTrue(True)
