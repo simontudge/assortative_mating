5574bfaddce91be63364181a172f5522800a6198
Simon Tudge <sjtudge@gmail.com>
2015-12-16 19:50:48 +0000

    Set matplotlib backend for non-graphical server

--- assortative_mating/__init__.py
+++ assortative_mating/__init__.py
@@ -1,3 +1,8 @@
+##Set the matplotlib backend to be suitable for a server without a graphical interface
+import matplotlib as mpl
+mpl.use('Agg')
+
+
 ##Expose the most important classes to the global namespace
 from .objects.individual import Individual
 from .objects.population import Population
